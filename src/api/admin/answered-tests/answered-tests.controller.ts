b1a613be59af110baec9b385db9d6d3d0a17caa3
Matin-PC <matink1490@gmail.com>
2024-12-14 13:10:43 +0330

    Fix bugs.

--- src/api/admin/answered-tests/answered-tests.controller.ts
+++ src/api/admin/answered-tests/answered-tests.controller.ts
@@ -12,13 +12,20 @@ export class AnsweredTestsController extends BaseController {
   async getList() {
     return await this.prisma.$queryRawUnsafe(`
         select at.id,
-               m.id     memberId,
+               m.id                          memberId,
+               concat(m.name, ' ', m.family) fullName,
+               m.schoolName,
+               m.educationLevel,
+               c.title                       city,
+               m.zone,
                m.mobileNumber,
-               pt.title testTitle,
+               pt.title                      testTitle,
                at.creationTime
         from member_answered_tests at
-            inner join members m
-        on m.id = at.userId inner join published_tests pt on pt.id = at.publishedTestItemId
+         inner join members m
+        on m.id = at.userId
+            inner join published_tests pt on pt.id = at.publishedTestItemId
+            left join cities c on c.cityId = m.city
     `);
   }
 
