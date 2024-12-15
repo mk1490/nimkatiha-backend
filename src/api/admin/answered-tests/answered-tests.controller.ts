16eafe8d65522b8f4533e38a59254ce37d9fcc6b
Matin-Laptop <matink1490@gmail.com>
2024-12-15 11:16:17 +0330

    Add download excel.

--- src/api/admin/answered-tests/answered-tests.controller.ts
+++ src/api/admin/answered-tests/answered-tests.controller.ts
@@ -29,6 +29,27 @@ export class AnsweredTestsController extends BaseController {
     `);
   }
 
+  @Get('/download-excel')
+  async downloadExcel() {
+    return await this.prisma.$queryRawUnsafe(`
+        select at.id,
+               m.id                          memberId,
+               concat(m.name, ' ', m.family) fullName,
+               m.schoolName,
+               m.educationLevel,
+               c.title                       city,
+               m.zone,
+               m.mobileNumber,
+               pt.title                      testTitle,
+               at.creationTime
+        from member_answered_tests at
+         inner join members m
+        on m.id = at.userId
+            inner join published_tests pt on pt.id = at.publishedTestItemId
+            left join cities c on c.cityId = m.city
+    `);
+  }
+
 
   @Get('/:id')
   async getDetails(
