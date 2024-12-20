ad2960d6073e034efd22d1162d08191601a5542b
Matin-PC <matink1490@gmail.com>
2024-12-20 19:26:29 +0330

    Fix bugs.

--- src/api/admin/answered-tests/answered-tests.controller.ts
+++ src/api/admin/answered-tests/answered-tests.controller.ts
@@ -31,23 +31,39 @@ export class AnsweredTestsController extends BaseController {
 
   @Get('/download-excel')
   async downloadExcel() {
-    return await this.prisma.$queryRawUnsafe(`
+    const items: any[] = await this.prisma.$queryRawUnsafe(`
         select at.id,
-               m.id                          memberId,
-               concat(m.name, ' ', m.family) fullName,
+               m.id     memberId,
+               m.name,
+               m.family,
+               m.nationalCode,
                m.schoolName,
                m.educationLevel,
-               c.title                       city,
+               c.title  city,
                m.zone,
                m.mobileNumber,
-               pt.title                      testTitle,
-               at.creationTime
+               pt.title testTitle,
+               at.creationTime,
+               at.stringifyData
         from member_answered_tests at
          inner join members m
         on m.id = at.userId
             inner join published_tests pt on pt.id = at.publishedTestItemId
             left join cities c on c.cityId = m.city
     `);
+
+
+    return items.map(f => {
+      const stringifyData = JSON.parse(f.stringifyData);
+      let totalScore = 0;
+      f.stringifyData = stringifyData.map(f => {
+        f.id = this.helper.generateUuid();
+        totalScore += f.score;
+        return f;
+      });
+      f.totalScore = totalScore;
+      return f;
+    });
   }
 
 
