157849a71eacfea46e4f5c828ad22921b79f40e8
Matin-Laptop <matink1490@gmail.com>
2025-01-27 21:16:55 +0330

    Fix bugs.

--- src/api/auth/dto/check-mobile-number-dto.ts
+++ src/api/auth/dto/check-mobile-number-dto.ts
@@ -2,9 +2,8 @@ import {ApiProperty} from "@nestjs/swagger";
 import {Matches} from "class-validator";
 
 export class CheckMobileNumberDto {
-
     @ApiProperty()
-    @Matches(/^09[0|1|2|3][0-9]{8}$/, {
+    @Matches(/^(([0-9]*)|(([1-9]*)\\.([0-9]*)))$/, {
         message: 'شماره همراه وارد شده معتبر نیست!',
         always: true,
     }, )
