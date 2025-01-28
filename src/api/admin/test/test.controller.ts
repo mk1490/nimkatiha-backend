fb7fedb6048045ec491edfd14df9d379463045f7
Matin-PC <matink1490@gmail.com>
2025-01-28 17:49:37 +0330

    Fix bugs.

--- src/api/admin/test/test.controller.ts
+++ src/api/admin/test/test.controller.ts
@@ -1,6 +1,8 @@
-import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put, Req, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { BaseController } from '../../../base/base-controller';
 import { CreateUpdateTestDto } from './dto/create-update-test-dto';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { memoryStorage } from 'multer';
 
 @Controller('test')
 export class TestController extends BaseController {
@@ -55,6 +57,134 @@ export class TestController extends BaseController {
           parentId: item.id,
         },
       }),
+    };
+  }
+
+
+  @UseInterceptors(FileInterceptor('file', {
+    storage: memoryStorage(),
+  }))
+  @Post('/insert-many/:parentTestId')
+  async insertManyFromExcel(
+    @Param('parentTestId') parentTestId,
+    @UploadedFile() file: Express.Multer.File, @Req() { user }) {
+    const excelToJson = require('convert-excel-to-json');
+    const json = excelToJson({
+      source: file.buffer,
+    });
+
+    function getKeyByValue(object, value) {
+      return Object.keys(object).find(key => object[key] === value);
+    }
+
+
+    let totalUpdate = 0, totalInsert = 0, totalFailed = 0;
+
+    const items = Object.entries(json)[0][1] as any[];
+    for (let i: number = 1; i < items.length; i++) {
+      const item = items[i];
+      const questionText = item[getKeyByValue(items[0], 'متن سوال')] || null;
+      const questionScore = Number(item[getKeyByValue(items[0], 'نمره')]) || 0;
+      let correctAnswer = item[getKeyByValue(items[0], 'گزینه صحیح')] || null;
+      const questionOne = {
+        label: item[getKeyByValue(items[0], 'الف')] || null,
+        id: this.helper.generateUuid(),
+      };
+      const questionTwo = {
+        label: item[getKeyByValue(items[0], 'ب')] || null,
+        id: this.helper.generateUuid(),
+      };
+      const questionThree = {
+        label: item[getKeyByValue(items[0], 'ج')] || null,
+        id: this.helper.generateUuid(),
+      };
+      const questionFour = {
+        label: item[getKeyByValue(items[0], 'د')] || null,
+        id: this.helper.generateUuid(),
+      };
+
+      let questionType = item[getKeyByValue(items[0], 'نوع سوال')] || null;
+
+      if (questionType == 'چهار گزینه ای') {
+        questionType = 1;
+      }
+
+      const transactions = [];
+      try {
+
+        const questionId = this.helper.generateUuid();
+
+        transactions.push(this.prisma.test_question_answer_items.createMany({
+          data: [
+            {
+              label: questionOne.label,
+              value: questionOne.id,
+              parentTestQuestionId: questionId,
+            },
+            {
+              label: questionTwo.label,
+              value: questionTwo.id,
+              parentTestQuestionId: questionId,
+            },
+            {
+              label: questionThree.label,
+              value: questionThree.id,
+              parentTestQuestionId: questionId,
+            },
+            {
+              label: questionFour.label,
+              value: questionFour.id,
+              parentTestQuestionId: questionId,
+            },
+          ],
+        }));
+
+        let correctAnswerId: string = null;
+        switch (correctAnswer) {
+          case 'الف':
+            correctAnswerId = questionOne.id;
+            break;
+          case 'ب':
+            correctAnswerId = questionTwo.id;
+            break;
+          case 'ج':
+            correctAnswerId = questionThree.id;
+            break;
+          case 'د':
+            correctAnswerId = questionFour.id;
+            break;
+        }
+
+
+        transactions.push(this.prisma.test_questions.create({
+          data: {
+            id: questionId,
+            questionScore,
+            questionTitle: questionText,
+            questionType: questionType,
+            parentId: parentTestId,
+            correctAnswerId,
+          },
+        }));
+        await this.prisma.$transaction(transactions);
+        totalInsert++;
+      } catch (e) {
+        totalFailed++;
+      }
+    }
+
+    const addedItems = await this.prisma.test_questions.findMany({
+      where: {
+        parentId: parentTestId,
+      },
+    });
+
+
+    return {
+      totalInsert,
+      totalUpdate,
+      totalFailed,
+      items: addedItems,
     };
   }
 
